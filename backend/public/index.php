e4d87a07610325b8203332a51553a604986a0c5c
pedroholiveira1998 <pdr_castro@hotmail.com>
2024-06-10 14:18:19 -0300

    feat: add config of routes

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo "API";
+require_once '../vendor/autoload.php';
+require_once '../src/utils/functions.php';
+require_once '../config/dotenv.php';
+
+require_once __DIR__ . '/../src/Routes/include.php';
